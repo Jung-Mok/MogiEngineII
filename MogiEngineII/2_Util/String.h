4d2d78fc43f7aec56ae70132bce1a8e39e4e8e97
Dotmok <dotmok82@gmail.com>
2020-03-05 08:28:18 +0900

    스트링 유니코드 변환

--- MogiEngineII/2_Util/String.h
+++ MogiEngineII/2_Util/String.h
@@ -25,6 +25,24 @@ namespace Mogi
 
 		static bool ConvertToUnicode(IN std::string const& strSrc, OUT std::wstring& strWDst)
 		{
+			size_t const src_size = strSrc.size();
+			size_t const BUFFER_SIZE = (src_size * 4);
+
+			if (0 < src_size)
+			{
+				wchar_t* pUNIBuffer = new wchar_t[BUFFER_SIZE];
+
+				size_t size = 0;
+				errno_t const err = ::mbstowcs_s(&size, pUNIBuffer, BUFFER_SIZE, strSrc.c_str(), BUFFER_SIZE);
+				if (!err && size)
+				{
+					strWDst = pUNIBuffer;
+				}
+
+				delete[] pUNIBuffer;
+				return err;
+			}
+
 			return true;
 		}
 
