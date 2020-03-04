61f26860440dbbc8cfb8e8c947636d585c827a37
Dotmok <dotmok82@gmail.com>
2020-03-05 08:30:50 +0900

    스트링 함수 정리

--- MogiEngineII/2_Util/String.cpp
+++ MogiEngineII/2_Util/String.cpp
@@ -12,5 +12,40 @@ namespace Mogi
 
 	}
 
+	std::wstring cString::GetWString()
+	{
+		std::wstring wstrRet;
+		Mogi::cString::ConvertToUnicode(m_String, wstrRet);
+		return wstrRet;
+	}
+
+	bool cString::ConvertToUnicode(IN std::string const& strSrc, OUT std::wstring& strWDst)
+	{
+		size_t const src_size = strSrc.size();
+		size_t const BUFFER_SIZE = (src_size * 4);
+
+		if (0 < src_size)
+		{
+			wchar_t* pUNIBuffer = new wchar_t[BUFFER_SIZE];
+
+			size_t size = 0;
+			errno_t const err = ::mbstowcs_s(&size, pUNIBuffer, BUFFER_SIZE, strSrc.c_str(), BUFFER_SIZE);
+			if (!err && size)
+			{
+				strWDst = pUNIBuffer;
+			}
+
+			delete[] pUNIBuffer;
+			return err;
+		}
+
+		return true;
+	}
+
+	bool cString::ComvertToMultiByte(IN std::wstring const& strWSrc, OUT std::string& strDst)
+	{
+		return true;
+	}
+
 }
 
--- MogiEngineII/2_Util/String.h
+++ MogiEngineII/2_Util/String.h
@@ -9,47 +9,18 @@ namespace Mogi
 		virtual ~cString();
 
 	public:
-		const char*									c_str()										{ return m_String.c_str(); }
-		size_t										size()										{ return m_String.size(); }
+		const char*									c_str()										{ return m_String.c_str();		}
+		size_t										size()										{ return m_String.size();		}
 
-
-		std::wstring								GetWString()
-		{
-			std::wstring wstrRet;
-			Mogi::cString::ConvertToUnicode(m_String, wstrRet);
-			return wstrRet;
-		}
+		std::wstring								GetWString();
 		
 
 
-
-		static bool ConvertToUnicode(IN std::string const& strSrc, OUT std::wstring& strWDst)
-		{
-			size_t const src_size = strSrc.size();
-			size_t const BUFFER_SIZE = (src_size * 4);
-
-			if (0 < src_size)
-			{
-				wchar_t* pUNIBuffer = new wchar_t[BUFFER_SIZE];
-
-				size_t size = 0;
-				errno_t const err = ::mbstowcs_s(&size, pUNIBuffer, BUFFER_SIZE, strSrc.c_str(), BUFFER_SIZE);
-				if (!err && size)
-				{
-					strWDst = pUNIBuffer;
-				}
-
-				delete[] pUNIBuffer;
-				return err;
-			}
-
-			return true;
-		}
-
-		static bool ComvertToMultiByte(IN std::wstring const& strWSrc, OUT std::string& strDst)
-		{
-			return true;
-		}
+	public:
+		// 멀티바이트를 유니코드로 전환
+		static bool									ConvertToUnicode(IN std::string const& strSrc, OUT std::wstring& strWDst);
+		// 유니코드를 멀티바이트로 전환
+		static bool									ComvertToMultiByte(IN std::wstring const& strWSrc, OUT std::string& strDst);
 
 	private:
 		std::string									m_String;
